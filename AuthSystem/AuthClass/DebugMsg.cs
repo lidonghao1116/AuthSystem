ffd9b82b2a9ee50e5d5e38e1792e951705fed80b
alwcelhome <suyongemail@qq.com>
2015-07-20 00:02:35 +0800

    添加DebugMsg

---
+++ AuthSystem/AuthClass/DebugMsg.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AuthSystem
+{
+    /// <summary>
+    /// 输出调试信息类！
+    /// </summary>
+    public class DebugMsg
+    {
+        static DebugMsg()
+        {
+        }
+        public static bool CloseDebugMsg;04189
+        /// <summary>
+        /// 默认路径与文件名
+        /// </summary>
+        private static string _filePath = "DebugMsg\\Debug" + DateTime.Now.Date.GetDateTimeFormats('d')[1].ToString() + ".txt";
+        /// <summary>
+        /// 保存设置信息的路径
+        /// </summary>
+        public static string filePath
+        {
+            get { return _filePath; }
+            set { _filePath = value; }
+        }
+        /// <summary>
+        /// 添加出错调试信息
+        /// </summary>
+        /// <param name="debugMessage">信息内容</param>
+        /// <param name="ClassName">标题或者类名或者方法名</param>
+        public static void Add(string debugMessage,string ClassName="Unknow")
+        {
+            try
+            {
+                if (!hasFile())
+                {
+                    AddFile();
+                }
+                System.IO.FileStream fs = System.IO.File.Open(filePath, System.IO.FileMode.Open);
+                byte[] tmpData = System.Text.Encoding.Default.GetBytes("[" + DateTime.Now.ToString() + "] ||| [" + ClassName + "]  [" + debugMessage + "]\r\n");
+                fs.Position = fs.Length;
+                fs.Write(tmpData, 0, tmpData.Length);
+                fs.Flush();
+                fs.Close();
+            }
+            catch (Exception x)
+            {
+                System.Diagnostics.Debug.WriteLine("DebugMsg.Add:"+x.Message);
+                throw;
+            }
+        }
+        /// <summary>
+        /// 添加出错调试信息
+        /// </summary>
+        /// <param name="exception">抛出的异常实体Exception</param>
+        public static void Add(Exception exception)
+        {
+            try
+            {
+                if (!hasFile())
+                {
+                    AddFile();
+                }
+                System.IO.FileStream fs = System.IO.File.Open(filePath, System.IO.FileMode.Open);
+                string tmpStr = exception.StackTrace.Remove(0, exception.StackTrace.LastIndexOf("在") - 1);
+                byte[] tmpData = System.Text.Encoding.Default.GetBytes("[" + DateTime.Now.ToString() + "] ||| [" + tmpStr + "]<...>[" + exception.Message + "]\r\n");
+                fs.Position = fs.Length;
+                fs.Write(tmpData, 0, tmpData.Length);
+                fs.Flush();
+                fs.Close();
+            }
+            catch (Exception x)
+            {
+                System.Diagnostics.Debug.WriteLine("DebugMsg.Add:" + x.Message);
+                throw;
+            }
+        }
+        /// <summary>
+        /// 检测是否存在输出文件
+        /// </summary>
+        /// <returns></returns>
+        public static bool hasFile()
+        {
+            try
+            {
+                if (System.IO.File.Exists(filePath))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        public static bool AddFile()
+        {
+            try
+            {
+                string tmpPath = filePath.Remove(filePath.LastIndexOf('\\'));
+                System.IO.Directory.CreateDirectory(tmpPath);
+                System.IO.File.Open(filePath, System.IO.FileMode.OpenOrCreate).Close();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ AuthSystem/AuthClass/SqlAction.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AuthSystem
+{
+    /// <summary>
+    /// SQL server 操作类
+    /// </summary>
+    public sealed class SqlAction
+    {
+        #region ----初始化----
+        static SqlAction()
+        {
+        }
+        #endregion
+
+        #region 0---公共变量
+        public static string globalConnectionString = "";//默认连接字符串
+
+        #endregion
+
+        #region 1---取基础对象
+
+        #region SqlConnection
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库连接对象;
+        /// </summary>
+        /// <returns>返回SqlConnection</returns>
+        public static SqlConnection GetConnection()
+        {
+            try
+            {
+                SqlConnection tmpConn = new SqlConnection(globalConnectionString);
+                return tmpConn;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetConnection" + x.Message);
+                throw;
+            }
+        }
+
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库连接对象;
+        /// </summary>
+        /// <param name="connectionString">连接字符串</param>
+        /// <returns>返回SqlConnection</returns>
+        public static SqlConnection GetConnection(string connectionString)
+        {
+            try
+            {
+                SqlConnection tmpConn = new SqlConnection(connectionString);
+                return tmpConn;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetConnection" + x.Message);
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region SqlCommand
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库命令对象;
+        /// </summary>
+        /// <returns>返回SqlCommand</returns>
+        public static SqlCommand GetCommand()
+        {
+            try
+            {
+                SqlCommand tmpComm = GetConnection().CreateCommand();
+                return tmpComm;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetCommand" + x.Message);
+                throw;
+            }
+        }
+
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库命令对象;
+        /// </summary>
+        /// <param name="commandText">SQL命令</param>
+        /// <returns>返回SqlCommand</returns>
+        public static SqlCommand GetCommand(string commandText)
+        {
+            try
+            {
+                SqlCommand tmpComm = GetCommand();
+                tmpComm.CommandText = commandText;
+                return tmpComm;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetCommand" + x.Message);
+                throw;
+            }
+        }
+
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库命令对象;
+        /// </summary>
+        /// <param name="connectionString">连接字符串</param>
+        /// <param name="commandText">SQL命令</param>
+        /// <returns>返回SqlCommand</returns>
+        public static SqlCommand GetCommand(string connectionString,string commandText)
+        {
+            try
+            {
+                SqlCommand tmpComm = new SqlConnection(connectionString).CreateCommand();
+                tmpComm.CommandText = commandText;
+                return tmpComm;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetCommand" + x.Message);
+                throw;
+            }
+        }
+
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库命令对象;
+        /// </summary>
+        /// <param name="sqlConnection">数据库连接对象</param>
+        /// <returns>返回SqlCommand</returns>
+        public static SqlCommand GetCommand(SqlConnection sqlConnection)
+        {
+            try
+            {
+                SqlCommand tmpComm = sqlConnection.CreateCommand();
+                return tmpComm;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetCommand" + x.Message);
+                throw;
+            }
+        }
+
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库命令对象;
+        /// </summary>
+        /// <param name="sqlConnection">数据库连接对象</param>
+        /// <param name="commandText">SQL命令</param>
+        /// <returns>返回SqlCommand</returns>
+        public static SqlCommand GetCommand(SqlConnection sqlConnection, string commandText)
+        {
+            try
+            {
+                SqlCommand tmpComm = sqlConnection.CreateCommand();
+                tmpComm.CommandText = commandText;
+                return tmpComm;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetCommand" + x.Message);
+                throw;
+            }
+        }
+        #endregion
+
+        #region SqlDataReader
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库DataReader;
+        /// </summary>
+        /// <param name="commandText">SQL命令</param>
+        /// <returns>返回SqlDataReader</returns>
+        public static SqlDataReader GetDataReader(string commandText)
+        {
+            try
+            {
+                SqlDataReader tmpSDR = GetCommand(commandText).ExecuteReader();
+                return tmpSDR;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetDataReader" + x.Message);
+                throw;
+            }
+        }
+
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库DataReader;
+        /// </summary>
+        /// <param name="commandText">SQL命令</param>
+        /// <param name="commandBehavior">提供对查询结果和查询对数据库的影响的说明</param>
+        /// <returns>返回SqlDataReader</returns>
+        public static SqlDataReader GetDataReader(string commandText,CommandBehavior commandBehavior)
+        {
+            try
+            {
+                SqlDataReader tmpSDR = GetCommand(commandText).ExecuteReader(commandBehavior);
+                return tmpSDR;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetDataReader" + x.Message);
+                throw;
+            }
+        }
+
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库DataReader;
+        /// </summary>
+        /// <param name="commandText">SQL命令</param>
+        /// <param name="connectionString">连接字符串</param>
+        /// <returns>返回SqlDataReader</returns>
+        public static SqlDataReader GetDataReader(string commandText,string connectionString)
+        {
+            try
+            {
+                SqlDataReader tmpSDR = GetCommand(connectionString, commandText).ExecuteReader();
+                return tmpSDR;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetDataReader" + x.Message);
+                throw;
+            }
+        }
+
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库DataReader;
+        /// </summary>
+        /// <param name="commandText">SQL命令</param>
+        /// <param name="connectionString">连接字符串</param>
+        /// <param name="commandBehavior">提供对查询结果和查询对数据库的影响的说明</param>
+        /// <returns>返回SqlDataReader</returns>
+        public static SqlDataReader GetDataReader(string commandText, string connectionString, CommandBehavior commandBehavior)
+        {
+            try
+            {
+                SqlCommand tmpComm = GetCommand(connectionString, commandText);
+                SqlDataReader tmpSDR = tmpComm.ExecuteReader(commandBehavior);
+                return tmpSDR;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetDataReader" + x.Message);
+                throw;
+            }
+        }
+
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库DataReader;
+        /// </summary>
+        /// <param name="commandText">SQL命令</param>
+        /// <param name="sqlConnection">数据库连接对象</param>
+        /// <<returns>返回SqlDataReader</returns>
+        public static SqlDataReader GetDataReader(string commandText, SqlConnection sqlConnection)
+        {
+            try
+            {
+                SqlDataReader tmpSDR = GetCommand(sqlConnection, commandText).ExecuteReader();
+                return tmpSDR;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetDataReader" + x.Message);
+                throw;
+            }
+        }
+
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库DataReader;
+        /// </summary>
+        /// <param name="commandText">SQL命令</param>
+        /// <param name="sqlConnection">数据库连接对象</param>
+        /// <param name="commandBehavior">提供对查询结果和查询对数据库的影响的说明</param>
+        /// <returns>返回SqlDataReader</returns>
+        public static SqlDataReader GetDataReader(string commandText, SqlConnection sqlConnection, CommandBehavior commandBehavior)
+        {
+            try
+            {
+                SqlDataReader tmpSDR = GetCommand(sqlConnection, commandText).ExecuteReader(commandBehavior);
+                return tmpSDR;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetDataReader" + x.Message);
+                throw;
+            }
+        }
+        #endregion
+
+        #region SqlDataAdapter
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库DataAdapter;
+        /// </summary>
+        /// <param name="commandText">SQL命令</param>
+        /// <param name="connectionString">连接字符串</param>
+        /// <returns>返回SqlDataAdapter</returns>
+        public static SqlDataAdapter GetDataAdapter(string commandText,string connectionString)
+        {
+            try
+            {
+                SqlDataAdapter tmpSDA = new SqlDataAdapter(commandText, connectionString);
+                return tmpSDA;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetDataAdapter" + x.Message);
+                throw;
+            }
+        }
+
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库DataAdapter;
+        /// </summary>
+        /// <param name="sqlCommand">数据库命令对象</param>
+        /// <returns>返回SqlDataAdapter</returns>
+        public static SqlDataAdapter GetDataAdapter(SqlCommand sqlCommand)
+        {
+            try
+            {
+                SqlDataAdapter tmpSDA = new SqlDataAdapter(sqlCommand);
+                return tmpSDA;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetDataAdapter" + x.Message);
+                throw;
+            }
+        }
+
+        //==============================================================================================================
+        /// <summary>
+        /// 获取数据库DataAdapter;
+        /// </summary>
+        /// <param name="commandText">SQL命令</param>
+        /// <param name="sqlConnection">数据库连接对象</param>
+        /// <returns>返回SqlDataAdapter</returns>
+        public static SqlDataAdapter GetDataAdapter(string commandText,SqlConnection sqlConnection)
+        {
+            try
+            {
+                SqlDataAdapter tmpSDA = new SqlDataAdapter(commandText,sqlConnection);
+                return tmpSDA;
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("模块:GetDataAdapter" + x.Message);
+                throw;
+            }
+        }
+        #endregion
+
+        #region 
+
+        #endregion
+
+        //==============================================================================================================
+        //==============================================================================================================
+        //==============================================================================================================
+        //==============================================================================================================
+        //==============================================================================================================
+        //==============================================================================================================
+        //==============================================================================================================
+        //==============================================================================================================
+        
+
+        #endregion
+
+        #region 2---取服务器信息
+        public static string GetServerTime()
+        {
+            try
+            {
+                string tmp;
+                SqlCommand tmpComm = GetCommand("select sysdatetime()");
+                tmpComm.Connection.Open();
+                tmp = tmpComm.ExecuteScalar().ToString();
+
+                return tmp;
+            }
+            catch (Exception x)
+            {
+                DebugMsg.Add(x);
+                return null;
+            }
+        }
+        #endregion
+    }
+}
